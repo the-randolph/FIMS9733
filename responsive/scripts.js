6123b6bcabaade965def06b59fd7941b6575708d
the-randolph <trinh.randy@queensu.ca>
2023-03-05 22:51:53 -0500

    Fixes to menu and smooth scrolling added

--- responsive/scripts.js
+++ responsive/scripts.js
@@ -2,10 +2,12 @@ window.onscroll = function() {stickyNav()};
 
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
+var displayLink = document.getElementById("displayLink");
 
 function stickyNav() {
     if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
+        navbar.classList.add("sticky");
+        displayLink.style.display = "none";
     } 
     else {
         navbar.classList.remove("sticky");
@@ -13,7 +15,6 @@ function stickyNav() {
 }
         
 function myMenu() {
-    var displayLink = document.getElementById("displayLink");
     if (displayLink.style.display === "block") {
         displayLink.style.display = "none";
     } 
@@ -21,12 +22,3 @@ function myMenu() {
         displayLink.style.display = "block";
     }
 }
-
-function functionExpand(id) {
-          var x = document.getElementById(id);
-          if (x.className.indexOf("w3-show") == -1) {
-            x.className += " w3-show";
-          } else { 
-            x.className = x.className.replace(" w3-show", "");
-          }
-        }
