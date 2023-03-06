3e2dcdfe6633cbadf89c3312afc0a0d6ea469548
the-randolph <trinh.randy@queensu.ca>
2023-03-06 00:34:29 -0500

    temp removal of auto collapse on mobile view

--- responsive/scripts.js
+++ responsive/scripts.js
@@ -4,7 +4,28 @@ var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 var displayLink = document.getElementById("displayLink");
 
+var flag = 0;
+
 function stickyNav() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+    } 
+    else {
+        navbar.classList.remove("sticky");
+    }
+}
+
+function stickyNavMobile() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+        displayLink.style.display = "none";
+    } 
+    else {
+        navbar.classList.remove("sticky");
+    }
+}
+
+function stickyNavDesktop() {
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky");
         displayLink.style.display = "none";
@@ -22,3 +43,12 @@ function myMenu() {
         displayLink.style.display = "block";
     }
 }
+
+function functionExpand(id) {
+          var x = document.getElementById(id);
+          if (x.className.indexOf("w3-show") == -1) {
+            x.className += " w3-show";
+          } else { 
+            x.className = x.className.replace(" w3-show", "");
+          }
+        }
